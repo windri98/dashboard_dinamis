9051cc5113f1764efa3d62fadc59b4d2968199d1
windri98 <windrinofedri944@gmail.com>
2025-09-21 18:07:05 +0700

    update icon

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -136,12 +136,21 @@
                         <span class="menu-text">Kelola Menu</span>
                     </a>
                 </div>
+
                 <div class="sub-menu-item">
                     <a href="{{ route('settings.dynamic-tables.index') }}">
                         <i class="fa fa-table" aria-hidden="true"></i>
                         <span class="menu-text">Kelola Tabel</span>
                     </a>
                 </div>
+
+                <div class="sub-menu-item">
+                    <a href="#" style="display: flex; align-items: center; gap: 8px;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16.694 13.716a5.277 5.277 0 0 0 5.366-5.187a5.28 5.28 0 0 0-5.366-5.186c-.454 0-.906.055-1.347.165A4.93 4.93 0 0 0 10.882.75a4.855 4.855 0 0 0-4.9 4.342a4.38 4.38 0 0 0-4.043 4.3a4.4 4.4 0 0 0 4.471 4.322zm3.551 9.534v-6.534m-1.307 0h2.613m-2.613 6.534h2.613m-10.454 0v-2.614m.003 0h1.96a1.96 1.96 0 1 0 0-3.92H11.1zM1.949 23.25l.737-4.92a1.9 1.9 0 0 1 3.752 0l.738 4.92m-4.884-2.287h4.541"/></svg>
+                        <span class="menu-text">Kelola API</span>
+                    </a>
+                </div>
+
             </div>
         </div>
         
